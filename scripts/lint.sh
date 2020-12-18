c1a80e1e5a709a6c329e36d71473af8ef448311b
Paul Kiernan <paul.kiernan@reddit.com>
2020-12-18 15:35:16 -0800

    Adds autoblack action

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -7,12 +7,11 @@ REPO_ROOT_DIR=$(git rev-parse --show-toplevel)
 docker run \
     --rm \
     -e OUTPUT_DETAILS=detailed \
-    -e PYTHON_BLACK_CONFIG_FILE="pyproject.toml" \
     -e RUN_LOCAL=true \
     -e VALIDATE_ALL_CODEBASE=true \
     -e VALIDATE_DOCKERFILE=true \
     -e VALIDATE_JSON=true \
-    -e VALIDATE_PYTHON_BLACK=true \
+    -e VALIDATE_PYTHON=true \
     -e VALIDATE_YAML=true \
     -v "$REPO_ROOT_DIR":/tmp/lint \
     github/super-linter

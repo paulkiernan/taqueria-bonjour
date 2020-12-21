7ab93da72c78bded9239d14352bbe6e40a36bc48
Paul Kiernan <paulkiernan@users.noreply.github.com>
2020-12-21 15:01:28 -0800

    Updates dockerfile to use yarn for builds and adds custom header (#6)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
+import { Helmet } from 'react-helmet'
+
 import logo from './taco.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Taqueria Bonjour</title>
+        <link rel="canonical" href="http://bonjour.paulynomial.com/" />
+      </Helmet>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p

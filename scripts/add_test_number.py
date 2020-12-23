72b49822dde7a41a5cbeb0e4c88d7ac4a5f664cb
Paul Kiernan <paul.kiernan@reddit.com>
2020-12-22 22:19:28 -0800

    [backend] update add number script

--- scripts/add_test_number.py
+++ scripts/add_test_number.py
@@ -3,14 +3,19 @@
 
 import requests
 
+numbers = {
+    'Paul': ("+16463696726", "US"),
+}
+
 if __name__ == "__main__":
 
-    req = requests.post(
-        "http://localhost:8080/add",
-        data={
-            "name": "Paul",
-            "number": "+16463696726",
-        },
-    )
-
-    print(req.text)
+    for _name, (_number, _code) in numbers.items():
+        req = requests.post(
+            "http://localhost:8080/add",
+            json={
+                "name": _name,
+                "number": _number,
+                "code": _code,
+            },
+        )
+        print(req.text)

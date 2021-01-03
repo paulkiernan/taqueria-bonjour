3fe4ba938ce76efe91873b5c0fd0d91f8bd91ae1
Paul Kiernan <paulkiernan@users.noreply.github.com>
2021-01-02 22:20:52 -0800

    Feature/backend migration (#11)
    * Init backend folder migration
    
    * update scripts
    
    * update github action
    
    * fixup: Format Python code with Black
    
    Co-authored-by: autoblack <autoblack@users.noreply.github.com>

--- scripts/run_frontend.sh
+++ scripts/run_frontend.sh
@@ -4,12 +4,15 @@ set -eu
 
 REPO_ROOT_DIR=$(git rev-parse --show-toplevel)
 
-docker build -t taqueria-bonjour-frontend:latest "$REPO_ROOT_DIR/frontend" --file frontend/Dockerfile.prod
+docker \
+    build \
+    -t taqueria-bonjour-frontend:latest \
+    "$REPO_ROOT_DIR/frontend" \
+    --file frontend/Dockerfile.prod
 
 docker run \
     --rm \
     -v ${REPO_ROOT_DIR}/frontend:/src \
     -v /src/node_modules \
     -p 3000:80 \
-    -e CHOKIDAR_USEPOLLING=true \
     -it taqueria-bonjour-frontend:latest
